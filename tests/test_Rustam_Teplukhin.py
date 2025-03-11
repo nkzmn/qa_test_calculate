95e464175f4bd7e69c8d4f247e1976e13a85d2db
Rustam Teplukhin <teplukhinrg@student.bmstu.ru>
2025-03-11 09:46:07 +0300

    Change test file by Rustam

--- tests/test_Rustam_Teplukhin.py
+++ tests/test_Rustam_Teplukhin.py
@@ -3,8 +3,6 @@ from src.calc import calculate
 
 def test_calc_division(): # тестирование деления
     assert calculate(10, 2, '/') == 5
-def test_calc_division_2(): # тестирование деления
-    assert calculate(10, 0, '/') == "Ошибка: деление на ноль!"
     
 def test_calc_subtraction(): # тестирование вычитания
     assert calculate(10, 2, '-') == 8
@@ -14,6 +12,3 @@ def test_calc_multiplication(): # тестирование умножение
 #
 def test_calc_addition(): # тестирование cложения
     assert calculate(10, 2, '+') == 12
-
-def test_calc_operation(): # тестирование операции
-    assert calculate(10, 2, '?') == "Неверная операция"
