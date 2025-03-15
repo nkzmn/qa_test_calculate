400104e00feb9bcfdf64fcd56de4ac3ca1389103
dima-code <dmitry.eselidze@yandex.ru>
2025-03-15 11:54:00 +0300

    calc_test_eselidze

---
+++ tests/test_Dmitrii_Eselidze.py
@@ -0,0 +1,14 @@
+from src.calc import calculate
+
+
+def test_calc_division(): # тестирование деления
+    assert calculate(10, 2, '/') == 5
+
+ def test_calc_subtraction(): # тестирование вычитания
+     assert calculate(47282.4, 342, '-') == 46940.4
+
+ def test_calc_multiplication(): # тестирование умножение
+     assert calculate(23432, 908332, '*') == 21284035424
+
+ def test_calc_addition(): # тестирование cложение
+     assert calculate(100, 200, '+') == 300
